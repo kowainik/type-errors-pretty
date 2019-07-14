8d34678af2faeff071395aa1641b678626db1559
Dmitry Kovanikov <kovanikov@gmail.com>
2019-07-14 21:00:01 +0800

    Implement pretty type errors

--- README.md
+++ README.md
@@ -1,9 +1,44 @@
 # type-errors-pretty
 
+![pretty-bug](https://user-images.githubusercontent.com/4276606/61183911-eda86300-a679-11e9-9b8e-34c129469075.png)
 [![Build status](https://img.shields.io/travis/chshersh/type-errors-pretty.svg?logo=travis)](https://travis-ci.org/chshersh/type-errors-pretty)
 [![Hackage](https://img.shields.io/hackage/v/type-errors-pretty.svg?logo=haskell)](https://hackage.haskell.org/package/type-errors-pretty)
 [![Stackage Lts](http://stackage.org/package/type-errors-pretty/badge/lts)](http://stackage.org/lts/package/type-errors-pretty)
 [![Stackage Nightly](http://stackage.org/package/type-errors-pretty/badge/nightly)](http://stackage.org/nightly/package/type-errors-pretty)
 [![MPL-2.0 license](https://img.shields.io/badge/license-MPL--2.0-blue.svg)](LICENSE)
 
-Combinators for writing pretty type errors easily
+> "It is important that we forgive ourselves for making mistakes. We need to learn
+> from our errors and move on."
+>
+> ― Steve Maraboli, Life, the Truth, and Being Free
+
+Combinators for writing pretty type errors easily. The word **pretty** here
+doesn't mean that the resulting type errors will be pretty (though, I believe
+they will be awesome), but the way the type errors are defined in your code is
+pretty.
+
+If you're interested in motivation behind using type errors at first place, you
+can read the following blog post:
+
+* [A story told by Type Errors](https://kodimensional.dev/type-errors)
+
+The `type-errors-pretty` library allows you to write text of custom compile-time
+error messages with less hassle in the following way:
+
+```haskell
+import Type.Errors.Pretty (type (<>), type (%))
+
+
+type MessageText (e1 :: k) (e2 :: k) (es :: [k])
+    = "You require the following two effects from your computation:"
+    % ""
+    % "    '" <> e1 <> "' and '" <> e2 <> "'"
+    % ""
+    % "However, your monad is capable of performing only the following effects:"
+    % ""
+    % "    " <> es
+```
+
+## Acknowledgement
+
+Icons made by [Freepik](http://www.freepik.com) from [www.flaticon.com](https://www.flaticon.com/) is licensed by [CC 3.0 BY](http://creativecommons.org/licenses/by/3.0/).
---
+++ src/Type/Errors/Pretty.hs
@@ -0,0 +1,110 @@
+{-# LANGUAGE TypeFamilies  #-}
+{-# LANGUAGE TypeInType    #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UnicodeSyntax #-}
+
+{- |
+This module provides type-level functions and operators to make the job of
+writing text of custom type errors easier. The motivation behind using custom
+type errors is described in detail in the following blog post:
+
+* [A story told by Type Errors](https://kodimensional.dev/type-errors)
+
+If you want to write the text of a custom error message, you need to use
+constructors of the 'ErrorMessage' data type. But this gets messy and
+inconvenient pretty quickly. Consider the following examples:
+
+@
+__type__ MessageText (e1 :: k) (e2 :: k) (es :: [k]) =
+       ''Text' "You require the following two effects from your computation:"
+ '':$$:' ''Text' ""
+ '':$$:' ''Text' "    '" '':<>:' ''ShowType' e1 '':<>:' ''Text' "' and '" '':<>:' ''ShowType' e2 '':<>:' ''Text' "'"
+ '':$$:' ''Text' ""
+ '':$$:' ''Text' "However, your monad is capable of performing only the following effects:"
+ '':$$:' ''Text' ""
+ '':$$:' ''Text' "    " '':<>:' ''ShowType' es
+@
+
+Using combinators from this library, you can define error messages in a simpler
+way:
+
+@
+__type__ MessageText (e1 :: k) (e2 :: k) (es :: [k])
+    = "You require the following two effects from your computation:"
+    '%' ""
+    '%' "    '" 'Type.Errors.Pretty.<>' e1 'Type.Errors.Pretty.<>' "' and '" 'Type.Errors.Pretty.<>' e2 'Type.Errors.Pretty.<>' "'"
+    '%' ""
+    '%' "However, your monad is capable of performing only the following effects:"
+    '%' ""
+    '%' "    " 'Type.Errors.Pretty.<>' es
+@
+
+If you prefer, you can use unicode operators to contstruct messages:
+
+@
+__type__ MessageText (e1 :: k) (e2 :: k) (es :: [k])
+    = "You require the following two effects from your computation:"
+    '•' ""
+    '•' "    '" '⊕' e1 '⊕' "' and '" '⊕' e2 '⊕' "'"
+    '•' ""
+    '•' "However, your monad is capable of performing only the following effects:"
+    '•' ""
+    '•' "    " '⊕' es
+@
+-}
+
+module Type.Errors.Pretty
+       ( -- * Combinators
+         type (<>)
+       , type (⊕)
+       , type (%)
+       , type (•)
+
+         -- * Reexports from "GHC.TypeLits"
+       , TypeError
+
+         -- * Helper internal type families
+       , ToErrorMessage
+       ) where
+
+import GHC.TypeLits (ErrorMessage (..), Symbol, TypeError)
+
+
+{- | Append two types automatically converting them to corresponding
+'ErrorMessage' constructors.
+
+>>> :kind! "Integer values have type: " <> Int
+"Integer values have type: " <> Int :: ErrorMessage
+= 'Text "Integer values have type: " ':<>: 'ShowType Int
+-}
+infixl 5 <>
+type family (<>) (l :: k1) (r :: k2) :: ErrorMessage where
+    l <> r = ToErrorMessage l ':<>: ToErrorMessage r
+
+{- | Unicode version of the 'Type.Errors.Pretty.<>' type-level operator.
+-}
+infixl 5 ⊕
+type (⊕) (l :: k1) (r :: k2) = l <> r
+
+{- | Append two types on top of each other automatically converting them to
+corresponding 'ErrorMessage' constructors.
+
+>>> :kind! "Expecting value of type: " % "   " <> Integer
+"Expecting value of type: " % "   " <> Integer :: ErrorMessage
+= 'Text "Expecting value of type: "
+  ':$$: ('Text "   " ':<>: 'ShowType Integer)
+-}
+infixr 4 %
+type family (%) (t :: k1) (b :: k2) :: ErrorMessage where
+    t % b = ToErrorMessage t ':$$: ToErrorMessage b
+
+{- | Unicode version of the '%' type-level operator.
+-}
+infixr 4 •
+type (•) (t :: k1) (b :: k2) = t % b
+
+-- | Type family to convert any type to 'ErrorMessage'.
+type family ToErrorMessage (t :: k) :: ErrorMessage where
+    ToErrorMessage (t :: Symbol) = 'Text t
+    ToErrorMessage (t :: ErrorMessage) = t
+    ToErrorMessage t = 'ShowType t
